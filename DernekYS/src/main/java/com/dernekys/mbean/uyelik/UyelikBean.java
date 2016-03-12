644ee4716f1bf8c4dab1de8c844c2b48cb81cf95
uto <uto@acer>
2016-03-12 23:18:51 +0200

    Uyelik

--- DernekYS/src/main/java/com/dernekys/mbean/uyelik/UyelikBean.java
+++ DernekYS/src/main/java/com/dernekys/mbean/uyelik/UyelikBean.java
@@ -1,6 +1,37 @@
 package com.dernekys.mbean.uyelik;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import com.dernekys.entity.EBase;
+
 //Okutanlar
-public class UyelikBean {
+@ManagedBean(name = "uyelikBean")
+@ViewScoped
+public class UyelikBean extends EBase {
+
+	private static final long serialVersionUID = -7587014621665755109L;
+	
+	private String msg = "Hello world";
+
+	public void printMsg() {
+		System.out.println("Týk týk");
+	}
+	
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	
+
+
+
+	
+	
+	
 
 }
